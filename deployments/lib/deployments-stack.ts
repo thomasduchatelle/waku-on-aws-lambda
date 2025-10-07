eed56c15b36b774535a86f4f0b3e42e98c131ef9
Thomas Duchatelle <thomas.duchatelle@underwriteme.co.uk>
2025-10-07 23:53:55 +0100

    remove hono base project

--- deployments/lib/deployments-stack.ts
+++ deployments/lib/deployments-stack.ts
@@ -2,14 +2,15 @@ import * as cdk from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import * as lambda from 'aws-cdk-lib/aws-lambda'
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
+import * as path from 'node:path';
 
 export class MyAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
     const fn = new NodejsFunction(this, 'lambda', {
-      entry: '../dist/serve-aws-lambda.js',
-      handler: 'handler',
+      handler: 'serve-aws-lambda.handler',
+      code: lambda.Code.fromAsset('../dist'),
       runtime: lambda.Runtime.NODEJS_LATEST,
     })
     const fnUrl = fn.addFunctionUrl({
